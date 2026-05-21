cee2a521940ef5853fb79d1b24b49bf91d4ef48c
miraclemagicein <themagicein01@atomicmail.io>
2026-05-21 22:46:53 +0330

    Update ws.js

--- api/ws.js
+++ api/ws.js
@@ -1,19 +1,17 @@
-export const config = {
-  runtime: "edge",
-};
+export const runtime = "edge";
 
-const TARGET = "https://sixofwands.pages.dev";
+const TARGET = "https://sixofwands.pages.dev"; 
 
 export default async function handler(req) {
   const url = new URL(req.url);
 
   const target = TARGET + url.pathname + url.search;
 
-  const newReq = new Request(target, {
+  const newRequest = new Request(target, {
     method: req.method,
     headers: req.headers,
     body: req.body
   });
 
-  return fetch(newReq);
+  return fetch(newRequest);
 }
