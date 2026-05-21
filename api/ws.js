71555f054ecbe378231b2b64b2ec8fc4cc5aea7b
miraclemagicein <themagicein01@atomicmail.io>
2026-05-21 22:36:46 +0330

    Create ws.js

---
+++ api/ws.js
@@ -0,0 +1,19 @@
+export const config = {
+  runtime: "edge",
+};
+
+const TARGET = "https://mytunnel.workers.dev";
+
+export default async function handler(req) {
+  const url = new URL(req.url);
+
+  const target = TARGET + url.pathname + url.search;
+
+  const newReq = new Request(target, {
+    method: req.method,
+    headers: req.headers,
+    body: req.body
+  });
+
+  return fetch(newReq);
+}
